7e939eb741d97a39b174fad0410edcf98dad43bc
Carlos-Landerdahl <carloslanderdahl27@gmail.com>
2023-05-30 20:33:29 -0300

    feat: aula21 - Springboot init

---
+++ aula21-Spring/src/test/java/com/dh/aula21Spring/Aula21SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.aula21Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula21SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
